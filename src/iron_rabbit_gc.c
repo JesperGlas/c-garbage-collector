fc7231b3964c3b9b8e8d0ebfddb6ee05f87c5de4
jesperglas <jesper.glas@gmail.com>
2023-03-09 16:31:41 +0100

    Sequential allocation seems to work

--- src/iron_rabbit_gc.c
+++ src/iron_rabbit_gc.c
@@ -13,7 +13,6 @@ bool g_heap_init = false;
 size_t g_pagesize;
 header_st *g_heap;
 header_st *g_free;
-header_st *g_used;
 
 void IR_init()
 {
@@ -23,15 +22,12 @@ void IR_init()
 	// initialize heap
 	g_heap = malloc(g_pagesize);
 	g_heap->m_size = g_pagesize - sizeof(header_st);
-	g_heap->m_next = (void *) NULL;
+	g_heap->m_next = g_heap + sizeof(header_st);
 
 	// initialize free ptr
-	g_free = g_heap + sizeof(header_st);
-	g_free->m_size = g_pagesize - 2 * sizeof(header_st);
+	g_free = g_heap->m_next;
+	g_free->m_size = g_heap->m_size;
 	g_free->m_next = (void *) NULL;
-
-	// initalize used ptr
-	g_used = (void *) NULL;
 
 	// set heap as active
 	g_heap_init = true;
@@ -50,13 +46,52 @@ void IR_info()
 
 	// heap info
 	printf("\tCurrent size: %lu\n", g_heap->m_size);
-	printf("\t%p [0] Heap start\n", g_heap);
-	printf("\t%p [%lu] Free\n", g_free, g_free - g_heap);
+	printf("\t%p [0] Heap start\n", (void *) g_heap);
+
+	// print all free blocks
+	size_t i = 0;
+	for (header_st *ptr = g_free; ptr != (void *) NULL; ptr = ptr->m_next, i++)
+	{
+		printf(	"\t%p [%lu] Free block %lu (Size %lu)\n",
+			(void *) ptr,
+			ptr - g_heap,
+			i,
+			ptr->m_size
+		);
+	}
+
+	// print all used blocks
+	i = 0;
+	for (header_st *ptr = g_heap->m_next; ptr != g_free; ptr = ptr->m_next)
+	{
+		printf(	"\t%p [%lu] Used block %lu (Size %lu)\n",
+			(void *) ptr,
+			ptr - g_heap,
+			i,
+			ptr->m_size
+		);
+	}
 }
 
 void *IR_malloc(size_t bytes_size)
 {
-	IR_init();
+	// init heap on first call to IR_malloc
+	if (g_heap_init == false)
+		IR_init();
 
-	return (void *) NULL;
+	size_t chunk_size = sizeof(header_st) + bytes_size;
+
+	// save old free header
+	header_st *ptr = g_free;
+
+	// update free ptr and header
+	g_free += chunk_size;
+	g_free->m_size = ptr->m_size - chunk_size;
+	g_free->m_next = (void *) NULL;
+
+	// update ptr header
+	ptr->m_size = chunk_size;
+	ptr->m_next = g_free;
+
+	return ptr + sizeof(header_st);
 }
--- src/iron_rabbit_gc_demo.c
+++ src/iron_rabbit_gc_demo.c
@@ -9,7 +9,14 @@ int main()
 
 	IR_info();
 
-	IR_malloc(5);
+	int *ptr1 = (int *) IR_malloc(sizeof(int));
+	(*ptr1) = 5;
+	printf("Returned ptr1: %p (%d)\n", (void *) ptr1, (*ptr1));
+	IR_info();
+
+	int *ptr2 = (int *) IR_malloc(sizeof(int));
+	(*ptr2) = 10;
+	printf("Returned ptr2: %p (%d)\n", (void *) ptr2, (*ptr2));
 	IR_info();
 
 	return 0;
